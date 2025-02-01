23f7eed9a499a168ceced0ffd76b72626e9a5a99
codemoneng <zcoulibalyeng@gmail.com>
2025-01-31 23:32:56 -0500

    Implemented the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
     <main className="flex flex-col items-center  px-3 pt-36">
         <h1 className="text-3xl lg:text-6xl font-bold">Find events around you</h1>
         <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
-          Browse more than <span className="font-bold italic underline text-[#a4f839]"> 10,000 events</span>  around you
+          Browse more than <span className="font-bold italic underline text-accent"> 10,000 events</span>  around you
         </p>
 
         <form className="w-full sm:w-[580px]">
             <input
                 className="w-full h-16 rounded-lg
                 bg-white/[7%] px-6 outline-none
-                ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10"
+                ring-accent/50 transition focus:ring-2 focus:bg-white/10"
                 placeholder="Search events in city..."
                 spellCheck={false}
             />
