b1250e2c7980e15373875c7d7669136186e0dfd0
codemoneng <zcoulibalyeng@gmail.com>
2025-02-01 00:25:14 -0500

    implemented the search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,17 @@
 import Link from "next/link";
+import React from "react";
+import SearchForm from "@/components/search-form";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center  px-3 pt-36">
         <h1 className="text-3xl lg:text-6xl font-bold">Find events around you</h1>
         <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
-          Browse more than <span className="font-bold italic underline text-accent"> 10,000 events</span>  around you
+          Browse more than <span className="font-bold italic underline text-accent"> 10,000 events</span>{" "} around you
         </p>
 
-        <form className="w-full sm:w-[580px]">
-            <input
-                className="w-full h-16 rounded-lg
-                bg-white/[7%] px-6 outline-none
-                ring-accent/50 transition focus:ring-2 focus:bg-white/10"
-                placeholder="Search events in city..."
-                spellCheck={false}
-            />
-        </form>
+
+        <SearchForm />
 
         <section className='mt-4 flex gap-x-4 text-sm text-white/50'>
             <p>Popular:</p>
