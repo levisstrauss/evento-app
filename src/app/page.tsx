3889a4f220ba26c34d471e80382d835e1636a55d
codemoneng <zcoulibalyeng@gmail.com>
2025-02-01 00:33:54 -0500

    Implemented the H1 component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import React from "react";
 import SearchForm from "@/components/search-form";
+import H1 from "@/components/h1";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center  px-3 pt-36">
-        <h1 className="text-3xl lg:text-6xl font-bold">Find events around you</h1>
+
+        <H1>Find events around you</H1>
+
         <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
           Browse more than <span className="font-bold italic underline text-accent"> 10,000 events</span>{" "} around you
         </p>
-
 
         <SearchForm />
 
