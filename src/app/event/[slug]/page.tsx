98af7dc2b2ed95d0f1c89f6acb634d2eef79407e
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 13:45:16 -0500

    Finished the event page styling

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import H1 from "@/components/h1";
+import React from "react";
 
 type EventPageProps = {
     params: {
@@ -55,7 +56,6 @@ export default async function EventPage({params}: EventPageProps) {
                         <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
                             {event.name}
                         </H1>
-
                         <p className="
                             whitespace-nowrap
                             text-xl
@@ -74,13 +74,36 @@ export default async function EventPage({params}: EventPageProps) {
                         </button>
                     </div>
                 </div>
-
             </section>
 
-            <div>
+            <div className="min-h-[75vh] text-center px-5 py-16">
+                <Section>
+                    <SectionHeading>About this event</SectionHeading>
+                    <SectionContent>{event.description}</SectionContent>
+                </Section>
 
+                <Section>
+                    <SectionHeading>Location</SectionHeading>
+                    <SectionContent>{event.location}</SectionContent>
+                </Section>
             </div>
 
         </main>
     )
+}
+
+// Very important concept of separation of concern
+
+function Section({ children }: {children: React.ReactNode}) {
+   return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: {children: React.ReactNode}) {
+    return <h2 className="text-2xl mb-8">{children}</h2>
+}
+
+function SectionContent({ children }: {children: React.ReactNode}) {
+    return <p className="max-w-4xl mx-auto text-lg leading-8 text-white/75">
+        {children}
+    </p>
 }
