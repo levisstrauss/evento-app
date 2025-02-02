600df795a1f35d7f66e932c96eb5057c6fb2beb1
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 12:23:40 -0500

    Fetched Event from url

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
 
-export default function EventPage() {
+type EventPageProps = {
+    params: {
+        slug: string;
+    }
+}
+
+export default async function EventPage({params}: EventPageProps) {
+
+    const slug = params.slug
+    const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+    );
+
+    const data = await response.json();
+
+    console.log(data)
+
     return (
         <main>Event Page</main>
     )
