688f1b5a1aed72fee799b86b638666df7de772bc
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 17:42:55 -0500

    Setup SQLite with Prisma

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import {twMerge} from "tailwind-merge";
 import clsx, {ClassValue} from "clsx";
-import {EventoEvent} from "@/lib/types";
+import {EventoEvent} from "@prisma/client";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -25,7 +25,9 @@ export async function getEvent(slug: string){
     const response = await fetch(
         `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
     );
-    return await response.json();
+
+    const event: EventoEvent[] = await response.json();
+    return event
 }
 
 // export async function sleep(ms: number): Promise<void> {
