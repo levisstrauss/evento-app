68ebb980e186fa93215a07e41b6f79cb387c041f
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 15:33:13 -0500

    Skeleton card component

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Skeleton from "@/components/skeleton";
 
 export default function Loading() {
     return (
-        <div>Loading city...</div>
+        <div className="flex flex-col items-center gap-y-4 pt-28">
+            <Skeleton className="h0=-4 w-[550px]"/>
+            <Skeleton className="h0=-4 w-[400px]"/>
+            <Skeleton className="h0=-4 w-[430px]"/>
+        </div>
     )
 }
--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import SkeletonCard from "@/components/skeleton-card";
 
 export default function Loading() {
     return (
-        <div>Loading events...</div>
+        <div className="flex flex-wrap  justify-center max-w-[1100px] mx-auto px-[20px] py-24 gap-20">
+            {Array.from({ length: 6 }).map((_, i) => (
+                <SkeletonCard key={i} />
+            ))}
+        </div>
     )
 }
