b35afa40f97b4a3bfc2082b91c43a905ed2ed267
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 11:05:24 -0500

    Finished EventCard wrap in link, honver effect

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,5 +1,6 @@
 import {EventoEvent} from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 
 type EventCardProps = {
     event: EventoEvent
@@ -7,40 +8,45 @@ type EventCardProps = {
 
 export default function EventCard({event }: EventCardProps) {
     return (
-        <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px]
-        bg-white/[3%] rounded-xl overflow-hidden relative"
+        <Link className="flex-1 basis-80 h-[380px] max-w-[500px]"
+              href={`/event/${event.slug}`}
         >
-            <Image
-                src={event.imageUrl}
-                alt={event.name}
-                width={500}
-                height={280}
-                className="h-[60%] object-fit"
-            />
-            <div className="flex flex-col flex-1 justify-center items-center">
-                <h2 className="text-2xl font-semibold">{event.name}</h2>
-                <p className="italic text-white/75">{event.organizerName}</p>
-                <p className="text-sm text-white/50 mt-4">{event.location}</p>
-            </div>
-
-            <section className="absolute flex justify-center items-center
-               flex-col left-[12px] top-[12px] h-[45px] w-[45px]
-             bg-black/30 rounded-md"
+            <section className="
+                 w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden
+                 relative transition hover:scale-105 active:scale-[1.02]"
             >
-                <p className="text-xl font-bold -mb-[5px]">
-                    {
-                       new Date(event.date).toLocaleDateString("en-US", {
-                           day: "2-digit",
-                       })
-                    }
-                </p>
-                <p className="text-xs uppercase text-accent">
-                    {new Date(event.date).toLocaleDateString("en-US", {
-                        month: "short",
-                    })}
-                </p>
+                <Image
+                    src={event.imageUrl}
+                    alt={event.name}
+                    width={500}
+                    height={280}
+                    className="h-[60%] object-fit"
+                />
+                <div className="flex flex-col flex-1 justify-center items-center">
+                    <h2 className="text-2xl font-semibold">{event.name}</h2>
+                    <p className="italic text-white/75">{event.organizerName}</p>
+                    <p className="text-sm text-white/50 mt-4">{event.location}</p>
+                </div>
 
+                <section className="absolute flex justify-center items-center
+                   flex-col left-[12px] top-[12px] h-[45px] w-[45px]
+                 bg-black/30 rounded-md"
+                >
+                    <p className="text-xl font-bold -mb-[5px]">
+                        {
+                           new Date(event.date).toLocaleDateString("en-US", {
+                               day: "2-digit",
+                           })
+                        }
+                    </p>
+                    <p className="text-xs uppercase text-accent">
+                        {new Date(event.date).toLocaleDateString("en-US", {
+                            month: "short",
+                        })}
+                    </p>
+
+                </section>
             </section>
-        </section>
+        </Link>
     )
 }
