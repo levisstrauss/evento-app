34b747c179a73c51d21709618474e3411890151b
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 22:54:34 -0500

    Fixed H1 component

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -7,7 +7,7 @@ type H1Props = {
     className?: string
 }
 
-export default function H1({children, className }: H1Props) {
+export default function H1({children, className="" }: H1Props) {
     return (
         <h1 className={cn("text-3xl lg:text-6xl font-bold tracking-tight", className)}>
             {children}
