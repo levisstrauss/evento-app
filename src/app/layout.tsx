ac9794ca81e20dde1fb3c35ac7cd893278b67755
codemoneng <zcoulibalyeng@gmail.com>
2025-02-02 22:44:58 -0500

    Deployment to Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,14 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Container from "@/components/container";
 
-
 // const inter = Inter({ subsets: ['latin'] })
 
 const firaCode = Fira_Code({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Evento - Find events around you',
-  description: 'Browse more than 10,000 events worldwide',
+    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'),
+    title: 'Evento - Find events around you',
+    description: 'Browse more than 10,000 events worldwide',
 }
 
 export default function RootLayout({
